0c4fc946891ba8d7602eb3d1feae06c21bf3fe98
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-03 01:25:22 +0530

    refactor: updated controller import export

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 import getAnimeCategory from "./animeCategory.controller";
 import getAnimeSearch from "./animeSearch.controller";
+import getHomePage from "./homePage.controller";
 
-export { getAnimeCategory, getAnimeSearch };
+export { getAnimeCategory, getAnimeSearch, getHomePage };
