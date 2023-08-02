e758c74d15a5158003b8fb6d3729c2224503fe68
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-02 13:40:23 +0530

    feat: added anime router

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,12 @@ import express, {
   ErrorRequestHandler,
   Application,
 } from "express";
+
 import { config } from "dotenv";
 import morgan from "morgan";
 import createHttpError from "http-errors";
+
+import animeRouter from "./routes";
 
 config();
 const app: Application = express();
@@ -16,8 +19,20 @@ const PORT: number = Number(process.env.PORT) || 4000;
 app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
-  res.json("hi there");
+  res.send(`
+    <body style="font-family: sans-serif; background: #000; color: #FFF;">
+      <h3>Welcome to Zoro.to api ⚔️</h3>
+      <a 
+        style="color: #00AEDD;"
+        href="https://github.com/ghoshRitesh12/zoro.to-api#readme"
+        rel="noopener noreferer"
+      >
+        Visit docs for more into
+      </a>
+    </body>
+  `);
 });
+app.use("/anime", animeRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) =>
   next(createHttpError.NotFound())
@@ -33,5 +48,5 @@ const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`⚔ api @ http://localhost:${PORT}`);
+  console.log(`⚔️  api @ http://localhost:${PORT}`);
 });
