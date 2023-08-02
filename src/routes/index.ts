02ece0298402a84b3b97f1320ad5d1a0afb67bba
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-03 01:20:06 +0530

    feat(homeRoute): added home route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router, IRouter } from "express";
-import { getAnimeCategory, getAnimeSearch } from "../controllers";
+import { getAnimeCategory, getAnimeSearch, getHomePage } from "../controllers";
 
 const router: IRouter = Router();
+
+// /anime/home
+router.get("/home", getHomePage);
 
 // /anime/search?q=${query}&page=${page}
 router.get("/search", getAnimeSearch);
