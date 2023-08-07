5e58fba71800969c3e84d48640eca16363058286
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-07 14:47:59 +0530

    chore(utils): refactored constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+import env from "../config/env";
+
+export const USER_AGENT_HEADER = env.APP_SRC_USER_AGENT;
+export const ACCEPT_HEADER = env.APP_SRC_ACCEPT_HEADER;
+export const ACCEPT_ENCODING_HEADER = env.APP_SRC_ACCEPT_ENCODING_HEADER;
+
+export const SRC_BASE_URL = env.APP_SRC_BASE_URL;
+export const SRC_AJAX_URL = env.APP_SRC_AJAX_URL;
+export const SRC_HOME_URL = env.APP_SRC_HOME_URL;
+export const SRC_SEARCH_URL = env.APP_SRC_SEARCH_URL;
