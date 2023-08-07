c543ab83fbbe2c45a9065f75a80dd347aec79292
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-07 14:50:18 +0530

    refactor: added `errorHandler`

---
+++ src/config/errorHandler.ts
@@ -0,0 +1,11 @@
+import { ErrorRequestHandler } from "express";
+
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  const status = error?.status || 500;
+  res.status(status).json({
+    status,
+    message: error?.message || "Something Went Wrong",
+  });
+};
+
+export default errorHandler;
