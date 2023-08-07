464acd3620b71c4610e4e33492e070e07a33c8b1
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-07 20:39:17 +0530

    test: added homePage anime info test

---
+++ test/homePage.test.ts
@@ -0,0 +1,16 @@
+import { scrapeHomePage } from "../src/parsers";
+
+test("returns anime information present in homepage", async () => {
+  const data = await scrapeHomePage();
+
+  expect(data.spotlightAnimes).not.toEqual([]);
+  expect(data.trendingAnimes).not.toEqual([]);
+  expect(data.latestEpisodeAnimes).not.toEqual([]);
+  expect(data.topUpcomingAnimes).not.toEqual([]);
+  expect(data.topAiringAnimes).not.toEqual([]);
+  expect(data.genres).not.toEqual([]);
+
+  expect(data.top10Animes.today).not.toEqual([]);
+  expect(data.top10Animes.week).not.toEqual([]);
+  expect(data.top10Animes.month).not.toEqual([]);
+});
