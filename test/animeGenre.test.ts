7533b1e0b445349d521efc9ba03f77e24e4973cb
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-07 20:38:57 +0530

    test: added animeGenre test

---
+++ test/animeGenre.test.ts
@@ -0,0 +1,9 @@
+import { scrapeGenreAnime } from "../src/parsers";
+
+test("returns animes belonging to a genre", async () => {
+  const data = await scrapeGenreAnime("shounen", 2);
+
+  expect(data.animes).not.toEqual([]);
+  expect(data.genres).not.toEqual([]);
+  expect(data.topAiringAnimes).not.toEqual([]);
+});
