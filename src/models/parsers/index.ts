353c7bac56928c47c6384220f669ae0d8d39cffa
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:49:10 +0530

    chore(parserModels): updated parser types import export

--- src/models/parsers/index.ts
+++ src/models/parsers/index.ts
@@ -1,13 +1,17 @@
 import { ScrapedHomePage } from "./homePage";
 import { ScrapedGenreAnime } from "./animeGenre";
 import { ScrapedAnimeCategory } from "./animeCategory";
+import { ScrapedProducerAnime } from "./animeProducer";
 import { ScrapedAnimeAboutInfo } from "./animeAboutInfo";
 import { ScrapedAnimeSearchResult } from "./animeSearch";
+import { ScrapedAnimeSearchSuggestion } from "./animeSearchSuggestion";
 
 export {
   ScrapedHomePage,
   ScrapedGenreAnime,
   ScrapedAnimeCategory,
+  ScrapedProducerAnime,
   ScrapedAnimeAboutInfo,
   ScrapedAnimeSearchResult,
+  ScrapedAnimeSearchSuggestion,
 };
