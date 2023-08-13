f0f28d170065bce5b8f9d55b94c70ae730c8e046
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:27:35 +0530

    refactor: removed home controller

--- src/controllers/home.controller.ts
+++
@@ -1,8 +0,0 @@
-import { resolve } from "path";
-import { RequestHandler } from "express";
-
-const homePage: RequestHandler = (req, res) => {
-  res.sendFile(resolve(__dirname, "..", "..", "public/index.html"));
-};
-
-export default homePage;
