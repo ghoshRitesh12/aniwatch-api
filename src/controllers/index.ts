e3e8e9422e3e944a2eade46973aef8cca0673a06
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:47:44 +0530

    chore(controller): updated controller import export

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,15 +1,17 @@
-import homePage from "./home.controller";
 import getGenreAnime from "./animeGenre.controller";
 import getHomePageInfo from "./homePage.controller";
 import getAnimeSearch from "./animeSearch.controller";
 import getAnimeCategory from "./animeCategory.controller";
 import getAnimeAboutInfo from "./animeAboutInfo.controller";
+import getAnimeSearchSuggestion from "./animeSearchSuggestion.controller";
+import getProducerAnimes from "./animeProducer.controller";
 
 export {
-  homePage,
   getGenreAnime,
   getAnimeSearch,
   getHomePageInfo,
   getAnimeCategory,
+  getProducerAnimes,
   getAnimeAboutInfo,
+  getAnimeSearchSuggestion,
 };
