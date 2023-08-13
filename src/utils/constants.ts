e8b3630caffc911f9b722402056ec924d67dcfce
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:30:40 +0530

    refactor(env): added all envs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,12 @@
-import env from "../config/env";
+export const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
 
-export const USER_AGENT_HEADER = env.APP_SRC_USER_AGENT;
-export const ACCEPT_HEADER = env.APP_SRC_ACCEPT_HEADER;
-export const ACCEPT_ENCODING_HEADER = env.APP_SRC_ACCEPT_ENCODING_HEADER;
+export const USER_AGENT_HEADER =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.71 Safari/537.36";
 
-export const SRC_BASE_URL = env.APP_SRC_BASE_URL;
-export const SRC_AJAX_URL = env.APP_SRC_AJAX_URL;
-export const SRC_HOME_URL = env.APP_SRC_HOME_URL;
-export const SRC_SEARCH_URL = env.APP_SRC_SEARCH_URL;
+export const ACCEPT_HEADER =
+  "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+
+export const SRC_BASE_URL = "https://aniwatch.to";
+export const SRC_AJAX_URL = "https://aniwatch.to/ajax";
+export const SRC_HOME_URL = "https://aniwatch.to/home";
+export const SRC_SEARCH_URL = "https://aniwatch.to/search";
