1080a15d233c1f9a15b3200d6ccb102773031bd2
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:49:34 +0530

    chore(parser): updated parser import export

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -2,12 +2,16 @@ import scrapeHomePage from "./homePage";
 import scrapeGenreAnime from "./animeGenre";
 import scrapeAnimeSearch from "./animeSearch";
 import scrapeAnimeCategory from "./animeCategory";
+import scrapeProducerAnimes from "./animeProducer";
 import scrapeAnimeAboutInfo from "./animeAboutInfo";
+import scrapeAnimeSearchSuggestion from "./animeSearchSuggestion";
 
 export {
   scrapeHomePage,
   scrapeGenreAnime,
   scrapeAnimeSearch,
   scrapeAnimeCategory,
+  scrapeProducerAnimes,
   scrapeAnimeAboutInfo,
+  scrapeAnimeSearchSuggestion,
 };
