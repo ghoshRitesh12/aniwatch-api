de302d67fbe8e66ffaa1089b5c02e41a0380cb13
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-13 19:48:44 +0530

    chore(controllerModels): updated controller types import export

--- src/models/controllers/index.ts
+++ src/models/controllers/index.ts
@@ -1,16 +1,24 @@
-import { AnimeSearchQueryParams } from "./animeSearch";
-import { AnimeAboutInfoQueryParams } from "./animeAboutInfo";
-import { GenreAnimePathParams, GenreAnimeQueryParams } from "./animeGenre";
 import {
   CategoryAnimePathParams,
   CategoryAnimeQueryParams,
 } from "./animeCategory";
+import {
+  AnimeProducerPathParams,
+  AnimeProducerQueryParams,
+} from "./animeProducer";
+import { AnimeSearchQueryParams } from "./animeSearch";
+import { AnimeAboutInfoQueryParams } from "./animeAboutInfo";
+import { AnimeSearchSuggestQueryParams } from "./animeSearchSuggestion";
+import { GenreAnimePathParams, GenreAnimeQueryParams } from "./animeGenre";
 
 export {
   GenreAnimePathParams,
   GenreAnimeQueryParams,
   AnimeSearchQueryParams,
+  AnimeProducerPathParams,
   CategoryAnimePathParams,
+  AnimeProducerQueryParams,
   CategoryAnimeQueryParams,
   AnimeAboutInfoQueryParams,
+  AnimeSearchSuggestQueryParams,
 };
