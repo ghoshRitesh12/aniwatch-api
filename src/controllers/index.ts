eaa384a83e33ad820c82f3860ccd88e40d5a0e33
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-17 23:24:47 +0530

    refactor(controller): updated controllers import export

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,17 +1,21 @@
 import getGenreAnime from "./animeGenre.controller";
 import getHomePageInfo from "./homePage.controller";
 import getAnimeSearch from "./animeSearch.controller";
+import getAnimeEpisodes from "./animeEpisodes.controller";
 import getAnimeCategory from "./animeCategory.controller";
-import getAnimeAboutInfo from "./animeAboutInfo.controller";
-import getAnimeSearchSuggestion from "./animeSearchSuggestion.controller";
 import getProducerAnimes from "./animeProducer.controller";
+import getAnimeAboutInfo from "./animeAboutInfo.controller";
+import getAnimeEpisodeSources from "./animeEpisodeSrcs.controller";
+import getAnimeSearchSuggestion from "./animeSearchSuggestion.controller";
 
 export {
   getGenreAnime,
   getAnimeSearch,
   getHomePageInfo,
+  getAnimeEpisodes,
   getAnimeCategory,
   getProducerAnimes,
   getAnimeAboutInfo,
+  getAnimeEpisodeSources,
   getAnimeSearchSuggestion,
 };
