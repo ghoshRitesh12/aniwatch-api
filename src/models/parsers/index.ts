d00da98fd94c009aef2c7424ee184c1cec7f24ec
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-17 23:23:03 +0530

    refactor(parserTypes): updated parser types import export

--- src/models/parsers/index.ts
+++ src/models/parsers/index.ts
@@ -1,17 +1,21 @@
 import { ScrapedHomePage } from "./homePage";
 import { ScrapedGenreAnime } from "./animeGenre";
+import { ScrapedAnimeEpisodes } from "./animeEpisodes";
 import { ScrapedAnimeCategory } from "./animeCategory";
 import { ScrapedProducerAnime } from "./animeProducer";
 import { ScrapedAnimeAboutInfo } from "./animeAboutInfo";
 import { ScrapedAnimeSearchResult } from "./animeSearch";
+import { ScrapedAnimeEpisodesSources } from "./animeEpisodeSrcs";
 import { ScrapedAnimeSearchSuggestion } from "./animeSearchSuggestion";
 
 export {
   ScrapedHomePage,
   ScrapedGenreAnime,
+  ScrapedAnimeEpisodes,
   ScrapedAnimeCategory,
   ScrapedProducerAnime,
   ScrapedAnimeAboutInfo,
   ScrapedAnimeSearchResult,
+  ScrapedAnimeEpisodesSources,
   ScrapedAnimeSearchSuggestion,
 };
