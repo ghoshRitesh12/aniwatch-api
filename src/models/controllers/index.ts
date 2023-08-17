bba1349edbbbca1dceb4bae34ffd4295f8b60cdc
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-17 23:50:23 +0530

    refactor(controllerModels): updated controller models import export

--- src/models/controllers/index.ts
+++ src/models/controllers/index.ts
@@ -7,7 +7,9 @@ import {
   AnimeProducerQueryParams,
 } from "./animeProducer";
 import { AnimeSearchQueryParams } from "./animeSearch";
+import { AnimeEpisodePathParams } from "./animeEpisodes";
 import { AnimeAboutInfoQueryParams } from "./animeAboutInfo";
+import { AnimeEpisodeSrcsQueryParams } from "./animeEpisodeSrcs";
 import { AnimeSearchSuggestQueryParams } from "./animeSearchSuggestion";
 import { GenreAnimePathParams, GenreAnimeQueryParams } from "./animeGenre";
 
@@ -15,10 +17,12 @@ export {
   GenreAnimePathParams,
   GenreAnimeQueryParams,
   AnimeSearchQueryParams,
+  AnimeEpisodePathParams,
   AnimeProducerPathParams,
   CategoryAnimePathParams,
   AnimeProducerQueryParams,
   CategoryAnimeQueryParams,
   AnimeAboutInfoQueryParams,
+  AnimeEpisodeSrcsQueryParams,
   AnimeSearchSuggestQueryParams,
 };
