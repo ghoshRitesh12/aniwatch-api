dc4bf477c9e261f7a86c9c2542be0dac83c2a002
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-17 23:16:45 +0530

    refactor(parser): updated parser import export

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,17 +1,21 @@
 import scrapeHomePage from "./homePage";
 import scrapeGenreAnime from "./animeGenre";
 import scrapeAnimeSearch from "./animeSearch";
+import scrapeAnimeEpisodes from "./animeEpisodes";
 import scrapeAnimeCategory from "./animeCategory";
 import scrapeProducerAnimes from "./animeProducer";
 import scrapeAnimeAboutInfo from "./animeAboutInfo";
+import scrapeAnimeEpisodeSources from "./animeEpisodeSrcs";
 import scrapeAnimeSearchSuggestion from "./animeSearchSuggestion";
 
 export {
   scrapeHomePage,
   scrapeGenreAnime,
   scrapeAnimeSearch,
+  scrapeAnimeEpisodes,
   scrapeAnimeCategory,
   scrapeProducerAnimes,
   scrapeAnimeAboutInfo,
+  scrapeAnimeEpisodeSources,
   scrapeAnimeSearchSuggestion,
 };
