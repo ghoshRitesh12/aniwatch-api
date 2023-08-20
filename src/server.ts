ca60147a88f7f616e5371190259ffc9296c52ac2
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-20 13:35:14 +0530

    feat(cors): using cors config

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 
 import https from "https";
 import { resolve } from "path";
+import corsConfig from "./config/cors";
 import { ratelimit } from "./config/ratelimit";
 import errorHandler from "./config/errorHandler";
 import notFoundHandler from "./config/notFoundHandler";
@@ -15,6 +16,7 @@ const app: express.Application = express();
 const PORT: number = Number(process.env.PORT) || 4000;
 
 app.use(morgan("dev"));
+app.use(corsConfig);
 app.use(ratelimit);
 
 app.use(express.static(resolve(__dirname, "..", "public")));
