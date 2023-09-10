6e11137ce2d5bd3b35beb7b56904abc82b2f6f18
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-09-10 23:45:13 +0530

    perf(rateLimit): increased `windowMs` time

--- src/config/ratelimit.ts
+++ src/config/ratelimit.ts
@@ -1,7 +1,7 @@
 import { rateLimit } from "express-rate-limit";
 
 export const ratelimit = rateLimit({
-  windowMs: 10 * 60 * 1000,
+  windowMs: 30 * 60 * 1000,
   max: 70,
   legacyHeaders: true,
   standardHeaders: "draft-7",
