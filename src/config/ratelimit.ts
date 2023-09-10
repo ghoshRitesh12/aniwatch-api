6ebee632d7e894b31f7e1fc8aece4b9e8456542a
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-09-10 19:36:34 +0530

    feat: updated `ratelimit` config

--- src/config/ratelimit.ts
+++ src/config/ratelimit.ts
@@ -1,8 +1,8 @@
 import { rateLimit } from "express-rate-limit";
 
 export const ratelimit = rateLimit({
-  windowMs: 10 * 60 * 1000,
-  max: 100,
+  windowMs: 60 * 1000,
+  max: 10,
   legacyHeaders: false,
   standardHeaders: true,
   message: "Too many API requests, try again later",
