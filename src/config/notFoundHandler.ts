6573e8318abef3e4dbec4be5611421ff3418fd62
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-22 13:43:09 +0530

    refactor: add strict type imports

--- src/config/notFoundHandler.ts
+++ src/config/notFoundHandler.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from "express";
+import type { RequestHandler } from "express";
 import createHttpError from "http-errors";
 
 const notFoundHandler: RequestHandler = (req, res, next) => {
