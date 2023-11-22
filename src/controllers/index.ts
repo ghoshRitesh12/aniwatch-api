784a5850a6f305718c53a19ce1eb9391cf478373
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-22 13:45:08 +0530

    fix(ts): controller file imports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,13 @@
-import getGenreAnime from "./animeGenre.controller";
-import getHomePageInfo from "./homePage.controller";
-import getAnimeSearch from "./animeSearch.controller";
-import getAnimeEpisodes from "./animeEpisodes.controller";
-import getAnimeCategory from "./animeCategory.controller";
-import getProducerAnimes from "./animeProducer.controller";
-import getEpisodeServers from "./episodeServers.controller";
-import getAnimeAboutInfo from "./animeAboutInfo.controller";
-import getAnimeEpisodeSources from "./animeEpisodeSrcs.controller";
-import getAnimeSearchSuggestion from "./animeSearchSuggestion.controller";
+import getGenreAnime from "./animeGenre.controller.js";
+import getHomePageInfo from "./homePage.controller.js";
+import getAnimeSearch from "./animeSearch.controller.js";
+import getAnimeEpisodes from "./animeEpisodes.controller.js";
+import getAnimeCategory from "./animeCategory.controller.js";
+import getProducerAnimes from "./animeProducer.controller.js";
+import getEpisodeServers from "./episodeServers.controller.js";
+import getAnimeAboutInfo from "./animeAboutInfo.controller.js";
+import getAnimeEpisodeSources from "./animeEpisodeSrcs.controller.js";
+import getAnimeSearchSuggestion from "./animeSearchSuggestion.controller.js";
 
 export {
   getGenreAnime,
