3531e7299896a48a66bdeac0bf858a081f70d129
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-12-17 19:39:46 +0530

    refactor: parser models import export

--- src/models/parsers/index.ts
+++ src/models/parsers/index.ts
@@ -6,6 +6,7 @@ import type { ScrapedProducerAnime } from "./animeProducer.js";
 import type { ScrapedEpisodeServers } from "./episodeServers.js";
 import type { ScrapedAnimeAboutInfo } from "./animeAboutInfo.js";
 import type { ScrapedAnimeSearchResult } from "./animeSearch.js";
+import type { ScrapedEstimatedSchedule } from "./estimatedSchedule.js";
 import type { ScrapedAnimeEpisodesSources } from "./animeEpisodeSrcs.js";
 import type { ScrapedAnimeSearchSuggestion } from "./animeSearchSuggestion.js";
 
@@ -18,6 +19,7 @@ export type {
   ScrapedEpisodeServers,
   ScrapedAnimeAboutInfo,
   ScrapedAnimeSearchResult,
+  ScrapedEstimatedSchedule,
   ScrapedAnimeEpisodesSources,
   ScrapedAnimeSearchSuggestion,
 };
