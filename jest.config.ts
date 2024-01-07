47f1aa2c04824675e40d71482dfd27035ac99a9b
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-01-07 17:25:31 +0530

    refactor: remove jest config file

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testTimeout: 100000,
-  testEnvironment: "node",
-  testMatch: ["**/test/**/*.test.ts"],
-};
