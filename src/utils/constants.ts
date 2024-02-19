ade24843902e85534268fbd38037c04d6c91b939
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2024-02-19 23:09:44 +0530

    added env file
    Added .env for better management of domain name in case of takedown and a minor update in README

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 export const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
 
 export const USER_AGENT_HEADER =
@@ -6,7 +8,9 @@ export const USER_AGENT_HEADER =
 export const ACCEPT_HEADER =
   "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
 
-export const SRC_BASE_URL = "https://aniwatch.to";
-export const SRC_AJAX_URL = "https://aniwatch.to/ajax";
-export const SRC_HOME_URL = "https://aniwatch.to/home";
-export const SRC_SEARCH_URL = "https://aniwatch.to/search";
+const domain = process.env.DOMAIN || 'aniwatch.to'
+
+export const SRC_BASE_URL = `https://${domain}`;
+export const SRC_AJAX_URL = `https://${domain}/ajax`;
+export const SRC_HOME_URL = `https://${domain}/home`;
+export const SRC_SEARCH_URL = `https://${domain}/search`;
