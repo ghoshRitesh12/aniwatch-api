dec2c427e2356e695ce88f8a32ede25e9e2760ec
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-02-26 01:05:20 +0530

    refactor: update extractor imports

--- src/extractors/index.ts
+++ src/extractors/index.ts
@@ -1,5 +1,6 @@
 import StreamSB from "./streamsb.js";
 import StreamTape from "./streamtape.js";
 import RapidCloud from "./rapidcloud.js";
+import MegaCloud from "./megacloud.js";
 
-export { StreamSB, StreamTape, RapidCloud };
+export { StreamSB, StreamTape, RapidCloud, MegaCloud };
