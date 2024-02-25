74e9455890c176e51f89e59a9bc909cd959dc09f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-02-26 01:04:32 +0530

    feat: replace RapidCloud extractor for MegaCloud

--- src/parsers/animeEpisodeSrcs.ts
+++ src/parsers/animeEpisodeSrcs.ts
@@ -8,7 +8,12 @@ import axios, { AxiosError } from "axios";
 import { load, type CheerioAPI } from "cheerio";
 import createHttpError, { type HttpError } from "http-errors";
 import { type AnimeServers, Servers } from "../types/anime.js";
-import { RapidCloud, StreamSB, StreamTape } from "../extractors/index.js";
+import {
+  RapidCloud,
+  StreamSB,
+  StreamTape,
+  MegaCloud,
+} from "../extractors/index.js";
 import { type ScrapedAnimeEpisodesSources } from "../types/parsers/index.js";
 
 // vidtreaming -> 4
@@ -28,7 +33,7 @@ async function scrapeAnimeEpisodeSources(
       case Servers.VidStreaming:
       case Servers.VidCloud:
         return {
-          ...(await new RapidCloud().extract(serverUrl)),
+          ...(await new MegaCloud().extract(serverUrl)),
         };
       case Servers.StreamSB:
         return {
