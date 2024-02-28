c3ee76a06b7527548a4699db3d9fa55c800c767c
Ayush Choudhary <87753540+ayushch80@users.noreply.github.com>
2024-02-28 12:42:05 +0000

    Added `.env.example`

--- src/config/ratelimit.ts
+++ src/config/ratelimit.ts
@@ -1,8 +1,10 @@
 import { rateLimit } from "express-rate-limit";
+import { config } from "dotenv";
+config();
 
 export const ratelimit = rateLimit({
-  windowMs: 30 * 60 * 1000,
-  max: 70,
+  windowMs: Number(process.env.WINDOWMS) || 30 * 60 * 1000,
+  max: Number(process.env.MAX) || 70,
   legacyHeaders: true,
   standardHeaders: "draft-7",
   message: "Too many API requests, try again later",
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+import { config } from "dotenv";
+config();
+
 export const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
 
 export const USER_AGENT_HEADER =
@@ -6,7 +9,9 @@ export const USER_AGENT_HEADER =
 export const ACCEPT_HEADER =
   "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
 
-export const SRC_BASE_URL = "https://aniwatchtv.to";
+const DOMAIN = process.env.DOMAIN || 'aniwatchtv.to'
+
+export const SRC_BASE_URL = `https://${DOMAIN}`;
 export const SRC_AJAX_URL = `${SRC_BASE_URL}/ajax`;
 export const SRC_HOME_URL = `${SRC_BASE_URL}/home`;
 export const SRC_SEARCH_URL = `${SRC_BASE_URL}/search`;
