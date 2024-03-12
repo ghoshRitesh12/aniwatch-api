870fae700b56cc20010296387e3d9cda8330560c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-03-12 22:54:09 +0530

    feat: add json rate limit response; replace `max` with `limit`

--- src/config/ratelimit.ts
+++ src/config/ratelimit.ts
@@ -1,11 +1,17 @@
-import { rateLimit } from "express-rate-limit";
 import { config } from "dotenv";
+import createHttpError from "http-errors";
+import { rateLimit } from "express-rate-limit";
+
 config();
 
 export const ratelimit = rateLimit({
   windowMs: Number(process.env.WINDOWMS) || 30 * 60 * 1000,
-  max: Number(process.env.MAX) || 70,
+  limit: Number(process.env.MAX) || 50,
   legacyHeaders: true,
   standardHeaders: "draft-7",
-  message: "Too many API requests, try again later",
+  handler: function (_, __, next) {
+    next(
+      createHttpError.TooManyRequests("Too many API requests, try again later")
+    );
+  },
 });
