be672c542262b4ea494b960d05d8dc0447885997
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-03-25 19:00:18 +0530

    refactor: update exports

--- src/types/controllers/index.ts
+++ src/types/controllers/index.ts
@@ -1,35 +1,10 @@
-import type {
-  GenreAnimePathParams,
-  GenreAnimeQueryParams,
-} from "./animeGenre.js";
-import type {
-  CategoryAnimePathParams,
-  CategoryAnimeQueryParams,
-} from "./animeCategory.js";
-import type {
-  AnimeProducerPathParams,
-  AnimeProducerQueryParams,
-} from "./animeProducer.js";
-import type { AnimeSearchQueryParams } from "./animeSearch.js";
-import type { AnimeEpisodePathParams } from "./animeEpisodes.js";
-import type { EpisodeServersQueryParams } from "./episodeServers.js";
-import type { AnimeAboutInfoQueryParams } from "./animeAboutInfo.js";
-import type { AnimeEpisodeSrcsQueryParams } from "./animeEpisodeSrcs.js";
-import type { EstimatedScheduleQueryParams } from "./estimatedSchedule.js";
-import type { AnimeSearchSuggestQueryParams } from "./animeSearchSuggestion.js";
-
-export type {
-  GenreAnimePathParams,
-  GenreAnimeQueryParams,
-  AnimeSearchQueryParams,
-  AnimeEpisodePathParams,
-  AnimeProducerPathParams,
-  CategoryAnimePathParams,
-  AnimeProducerQueryParams,
-  CategoryAnimeQueryParams,
-  AnimeAboutInfoQueryParams,
-  EpisodeServersQueryParams,
-  AnimeEpisodeSrcsQueryParams,
-  EstimatedScheduleQueryParams,
-  AnimeSearchSuggestQueryParams,
-};
+export type * from "./animeGenre.js";
+export type * from "./animeCategory.js";
+export type * from "./animeProducer.js";
+export type * from "./animeSearch.js";
+export type * from "./animeEpisodes.js";
+export type * from "./episodeServers.js";
+export type * from "./animeAboutInfo.js";
+export type * from "./animeEpisodeSrcs.js";
+export type * from "./estimatedSchedule.js";
+export type * from "./animeSearchSuggestion.js";
