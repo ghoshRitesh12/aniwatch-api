44ee297c2d8d9ce8c7621a8ea15094e6fc4bd89d
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-07-15 20:16:15 +0530

    test: update estimated schedule test to use zero padded month in date string

--- test/estimatedSchedule.test.ts
+++ test/estimatedSchedule.test.ts
@@ -1,10 +1,14 @@
 import { expect, test } from "vitest";
 import { scrapeEstimatedSchedule } from "../src/parsers/index.js";
 
+function padZero(num: number) {
+  return num < 10 ? `0${num}` : num.toString();
+}
+
 test("returns estimated schedule anime release", async () => {
   const d = new Date();
   const data = await scrapeEstimatedSchedule(
-    `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+    `${d.getFullYear()}-${padZero(d.getMonth() + 1)}-${d.getDate()}`
   );
 
   expect(data.scheduledAnimes).not.toEqual([]);
