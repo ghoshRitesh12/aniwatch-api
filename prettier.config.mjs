9ae824b42c2665869e0a7bc964339ee6cc414d1a
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-10-29 13:12:45 +0530

    feat(prettier): add `prettier.config.mjs`

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  singleQuote: false,
+  arrowParens: "always",
+  trailingComma: "es5",
+  singleAttributePerLine: true,
+};
