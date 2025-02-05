eea680018a930c91c64051a8f3271d0d3860de30
Yusuf Tuncay <yusuf@tuncay.be>
2025-02-05 22:38:57 +0100

    Add new domain to allowed origins for CORS configuration

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ config();
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://127.0.0.1:5500", "https://letswatch.site", "*"];
+  : ["http://127.0.0.1:5500", "https://letswatch.site", "https://letswatch.one", "*"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
