0ce074e95ff16b8beea87877f00704c2156bf468
yusuftuncay <70652416+yusuftuncay@users.noreply.github.com>
2025-03-24 01:27:56 +0100

    Add Github domain to allowed domains

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ config();
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://127.0.0.1:5500", "https://letswatch.one", "https://yusuftuncay.github.io/LetsWatch", "*"];
+  : ["http://127.0.0.1:5500", "https://letswatch.one", "https://github.letswatch.one", "*"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
