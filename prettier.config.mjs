cc838023aee529eb91d22050d4f044c42c62ad93
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-04-14 23:59:26 +0530

    style: formatting changes

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,12 +2,12 @@
  * @type {import("prettier").Config}
  */
 export default {
-  semi: true,
-  tabWidth: 2,
-  useTabs: false,
-  printWidth: 80,
-  singleQuote: false,
-  arrowParens: "always",
-  trailingComma: "es5",
-  singleAttributePerLine: true,
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    singleQuote: false,
+    arrowParens: "always",
+    trailingComma: "es5",
+    singleAttributePerLine: true,
 };
