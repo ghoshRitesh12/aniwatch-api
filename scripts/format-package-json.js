89724ca755c6450d40566c051cd077aee4681d97
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-05-12 01:38:18 +0530

    refactor: simplify preCommit

--- scripts/format-package-json.js
+++ scripts/format-package-json.js
@@ -7,25 +7,11 @@ export function preCommit({ tag, version }) {
             return;
         }
 
-        // Parse the JSON string into an object
-        const json = JSON.parse(data);
+        console.log({ tag, version });
 
-        // Convert the object back to a JSON string
+        const json = JSON.parse(data);
         const jsonString = JSON.stringify(json, null, 4);
 
-        // Write the updated JSON string back to the file
         writeFileSync("package.json", jsonString);
     });
-
-    // exec("jq --indent 4 . package.json > tmp.json && mv tmp.json package.json", (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`exec error: ${error}`);
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         console.error(`stderr: ${stderr}`);
-    //         return;
-    //     }
-    //     console.log(`stdout: ${stdout}`);
-    // });
 }
