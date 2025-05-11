a4cb088e8cb0e18578e497dc74b80a80e5ec9449
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-05-12 01:36:06 +0530

    ci: update release workflow try19

--- scripts/format-package-json.js
+++ scripts/format-package-json.js
@@ -1,18 +1,31 @@
-import { exec } from "child_process";
-import { writeFileSync } from "fs";
+import { readFile, writeFileSync } from "fs";
 
 export function preCommit({ tag, version }) {
-    writeFileSync("tmp.json", "{}")
+    readFile("package.json", "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
 
-    exec("jq --indent 4 . package.json > tmp.json && mv tmp.json package.json", (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
+        // Parse the JSON string into an object
+        const json = JSON.parse(data);
+
+        // Convert the object back to a JSON string
+        const jsonString = JSON.stringify(json, null, 4);
+
+        // Write the updated JSON string back to the file
+        writeFileSync("package.json", jsonString);
     });
+
+    // exec("jq --indent 4 . package.json > tmp.json && mv tmp.json package.json", (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         return;
+    //     }
+    //     if (stderr) {
+    //         console.error(`stderr: ${stderr}`);
+    //         return;
+    //     }
+    //     console.log(`stdout: ${stdout}`);
+    // });
 }
