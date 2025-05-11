b6288a02d85c108a5103ec1de15a1fa0b98d526e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-05-11 13:07:17 +0530

    feat: add custom pino logger

---
+++ src/config/logger.ts
@@ -0,0 +1,26 @@
+import { env } from "./env.js";
+import { pino, type LoggerOptions } from "pino";
+
+const loggerOptions: LoggerOptions = {
+    redact: env.isProduction ? ["hostname"] : [],
+    level: "info",
+    transport: env.isDev
+        ? {
+              target: "pino-pretty",
+              options: {
+                  colorize: true,
+                  translateTime: "SYS:standard",
+              },
+          }
+        : undefined,
+    formatters: {
+        level(label) {
+            return {
+                level: label.toUpperCase(),
+            };
+        },
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+};
+
+export const logger = pino(loggerOptions);
