eb33f10478e2fc27c3821e589793b80de3a1617b
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-05-12 01:33:40 +0530

    fix: ensure tmp.json is created before formatting package.json

--- scripts/format-package-json.js
+++ scripts/format-package-json.js
@@ -1,6 +1,9 @@
 import { exec } from "child_process";
+import { writeFileSync } from "fs";
 
 export function preCommit({ tag, version }) {
+    writeFileSync("tmp.json", "{}")
+
     exec("jq --indent 4 . package.json > tmp.json && mv tmp.json package.json", (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
