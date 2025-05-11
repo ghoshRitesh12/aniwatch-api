9e7b52ff9408ee55ff3533fedad1b3d2eeb08fe3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-05-12 00:04:32 +0530

    test: fix failing nextEpisodeSchedule test case

--- __tests__/nextEpisodeSchedule.test.ts
+++ __tests__/nextEpisodeSchedule.test.ts
@@ -1,11 +1,20 @@
 import { expect, test } from "vitest";
 import { HiAnime } from "aniwatch";
 
+function padZero(num: number) {
+    return num < 10 ? `0${num}` : num.toString();
+}
+
 // npx vitest run nextEpisodeSchedule.test.ts
 test("returns anime next episode schedule", async () => {
     const hianime = new HiAnime.Scraper();
 
-    const animeId = "one-piece-100";
+    const d = new Date();
+    const scheduleData = await hianime.getEstimatedSchedule(
+        `${d.getFullYear()}-${padZero(d.getMonth() + 1)}-${padZero(d.getDate())}`
+    );
+
+    const animeId = scheduleData.scheduledAnimes[0].id!;
     const data = await hianime.getNextEpisodeSchedule(animeId);
 
     expect(data.airingISOTimestamp).not.toEqual(null);
